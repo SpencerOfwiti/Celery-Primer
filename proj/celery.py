cdb0718cb51563aa80a6746b5bfcb42d28c557ca
SpencerOfwiti <maxspencer56@gmail.com>
2020-08-25 16:30:57 +0300

    Add celery instance. - Connect to redis message broker. - Add addition, multiplication and summation tasks.

---
+++ proj/celery.py
@@ -0,0 +1,9 @@
+from celery import Celery
+
+app = Celery('proj',
+             broker='redis://localhost:6379',
+             backend='redis://localhost:6379',
+             include=['proj.tasks'])
+
+if __name__ == '__main__':
+    app.start()
